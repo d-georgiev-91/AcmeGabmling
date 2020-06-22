9e954546b993ac8a41feb921bbd009ddd42bfa23
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 20:38:09 +0300

    Added random generator

---
+++ src/AcmeGambling/AcmeGambling/Generator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AcmeGambling.Symbols;
+
+namespace AcmeGambling
+{
+    public class Generator
+    {
+        private readonly Random _random = new Random();
+
+        public Symbol GetRandomSymbol(IReadOnlyCollection<Symbol> symbols)
+        {
+            var totalAppearanceProbability = symbols.Sum(x => x.AppearanceProbability);
+            Symbol winningSector = null;
+
+            var rgn = _random.Next(0, totalAppearanceProbability);
+            var sum = 0;
+
+            foreach (var symbol in symbols)
+            {
+                sum += symbol.AppearanceProbability;
+                if (rgn < sum)
+                {
+                    winningSector = symbol;
+                    break;
+                }
+            }
+
+            return winningSector;
+        }
+    }
+}
