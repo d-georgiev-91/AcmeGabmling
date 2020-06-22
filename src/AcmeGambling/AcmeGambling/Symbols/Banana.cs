215a36ccf6ac3526d39b5dc6077ead67ea90a43c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 17:22:54 +0300

    Added Banana symbol

---
+++ src/AcmeGambling/AcmeGambling/Symbols/Banana.cs
@@ -0,0 +1,11 @@
+﻿namespace AcmeGambling.Symbols
+{
+    /// <inheritdoc cref="Symbol"/>
+    public class Banana : Symbol
+    {
+        public Banana() :
+            base("B", 0.6M, 35)
+        {
+        }
+    }
+}
