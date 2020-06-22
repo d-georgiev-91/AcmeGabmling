670741a41e4e167b77434c200ac464b2494a9757
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 16:57:46 +0300

    Added class for reel symbols

---
+++ src/AcmeGambling/AcmeGambling/Symbol.cs
@@ -0,0 +1,23 @@
+﻿namespace AcmeGambling
+{
+    /// <summary>
+    /// Class representing a reel symbol
+    /// </summary>
+    public abstract class Symbol
+    {
+        /// <summary>
+        /// Placeholder holding the character of the symbol
+        /// </summary>
+        public abstract string Character { get; protected set; }
+
+        /// <summary>
+        /// The win coefficient
+        /// </summary>
+        public abstract decimal WinCoefficient { get; protected set; }
+
+        /// <summary>
+        /// Appearance percentage
+        /// </summary>
+        public abstract byte AppearanceProbability { get; protected set; }
+    }
+}
