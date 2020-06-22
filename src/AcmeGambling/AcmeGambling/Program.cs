acc2ceba4e7036e353ec4383d6728a6000e396fb
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 17:31:56 +0300

    Added reading of balance

--- src/AcmeGambling/AcmeGambling/Program.cs
+++ src/AcmeGambling/AcmeGambling/Program.cs
@@ -6,6 +6,21 @@ namespace AcmeGambling
     {
         static void Main(string[] args)
         {
+            var balance = ReadBalance();
+            Console.WriteLine(balance);
+        }
+
+        private static decimal ReadBalance()
+        {
+            Console.WriteLine("Please deposit money you want to play with:");
+            var balance = decimal.Parse(Console.ReadLine());
+
+            if (balance <= 0)
+            {
+                throw new ArgumentException("Balance should more than 0");
+            }
+
+            return balance;
         }
     }
 }
