7f782794eed35264e16edaeef89c88a6725f492c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 17:25:01 +0300

    Added Pineapple

---
+++ src/AcmeGambling/AcmeGambling/Symbols/Pineapple.cs
@@ -0,0 +1,11 @@
+﻿namespace AcmeGambling.Symbols
+{
+    /// <inheritdoc cref="Symbol"/>
+    public class Pineapple : Symbol
+    {
+        public Pineapple() :
+            base("P", 0.8M, 15)
+        {
+        }
+    }
+}
