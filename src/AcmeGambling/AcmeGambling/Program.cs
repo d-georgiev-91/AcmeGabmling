78ec1a2fe872ceb01864a442ec05cc922bad1b41
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-22 17:41:00 +0300

    Added steak reading

--- src/AcmeGambling/AcmeGambling/Program.cs
+++ src/AcmeGambling/AcmeGambling/Program.cs
@@ -8,6 +8,17 @@ namespace AcmeGambling
         {
             var balance = ReadBalance();
             Console.WriteLine(balance);
+
+            while (balance >= 0)
+            {
+                // TODO: input steak till its valid
+                var steak = ReadSteak(balance);
+                
+                // TODO: Play
+                Console.WriteLine(steak);
+            }
+
+            Console.WriteLine(balance);
         }
 
         private static decimal ReadBalance()
@@ -22,5 +33,24 @@ namespace AcmeGambling
 
             return balance;
         }
+
+        private static decimal ReadSteak(decimal balance)
+        {
+            Console.WriteLine("Enter steak amount:");
+            var steak = decimal.Parse(Console.ReadLine());
+
+            if (steak <= 0)
+            {
+                throw new ArgumentException("Steak should more than 0");
+            }
+
+            if (steak > balance)
+            {
+                throw new ArgumentException("Steak should less or equal to balance");
+            }
+
+            return steak;
+        }
+
     }
 }
