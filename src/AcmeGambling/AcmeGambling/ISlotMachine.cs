5cf3ece78fa91b6262155692a721874105be9dcb
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-24 22:43:13 +0300

    Added SlotMachine class and interface

---
+++ src/AcmeGambling/AcmeGambling/ISlotMachine.cs
@@ -0,0 +1,15 @@
+﻿namespace AcmeGambling
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISlotMachine
+    {
+        /// <summary>
+        /// Spins the sloth machine reels
+        /// </summary>
+        /// <param name="steak">The steak</param>
+        /// <returns>The won amount of won reward</returns>
+        decimal Spin(decimal steak);
+    }
+}
---
+++ src/AcmeGambling/AcmeGambling/SlotMachine.cs
@@ -0,0 +1,12 @@
+﻿namespace AcmeGambling
+{
+    /// <inheritdoc cref="ISlotMachine"/>
+    public class SlotMachine : ISlotMachine
+    {
+        /// <inheritdoc cref="ISlotMachine.Spin"/>
+        public decimal Spin(decimal steak)
+        {
+            return 0m;
+        }
+    }
+}
