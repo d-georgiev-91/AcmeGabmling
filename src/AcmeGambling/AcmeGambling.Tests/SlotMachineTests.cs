abb6946d9a3121f22b1f9dd1ad58fdaaad508e3c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-24 23:22:54 +0300

    Added unit tests

---
+++ src/AcmeGambling/AcmeGambling.Tests/SlotMachineTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace AcmeGambling.Tests
+{
+    [TestFixture]
+    public class SlotMachineTests
+    {
+        private ISlotMachine _slotMachine;
+
+        [SetUp]
+        public void Setup()
+        {
+            _slotMachine = new SlotMachine();
+        }
+        
+        [Test]
+        public void SpinTest()
+        {
+            var reward  = _slotMachine.Spin(3);
+            Assert.That(reward, Is.Zero);
+        }
+    }
+}
