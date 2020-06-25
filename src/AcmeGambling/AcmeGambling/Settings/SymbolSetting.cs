cf37571bd36aec5ffea223c8ae900536cff6a21a
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-26 00:06:48 +0300

    Added symbols settings

---
+++ src/AcmeGambling/AcmeGambling/Settings/SymbolSetting.cs
@@ -0,0 +1,20 @@
+﻿namespace AcmeGambling.Settings
+{
+    public class SymbolSetting
+    {
+        /// <summary>
+        /// Placeholder holding the character of the symbol
+        /// </summary>
+        public string Character { get; set; }
+
+        /// <summary>
+        /// The win coefficient
+        /// </summary>
+        public decimal WinCoefficient { get; set; }
+
+        /// <summary>
+        /// Appearance percentage
+        /// </summary>
+        public byte AppearanceProbability { get; set; }
+    }
+}
---
+++ src/AcmeGambling/AcmeGambling/Settings/SymbolsSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace AcmeGambling.Settings
+{
+    public class SymbolsSettings
+    {
+        public SymbolSetting Apple { get; set; }
+
+        public SymbolSetting Banana { get; set; }
+
+        public SymbolSetting Pineapple { get; set; }
+
+        public SymbolSetting WildCard { get; set; }
+    }
+}
